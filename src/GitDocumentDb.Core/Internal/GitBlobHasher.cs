21658394daba3b5a839113a8f9d22b439fb639e6
Vitor Mello - Dev <vgmello-dev@Admins-MacBook-Air.local>
2026-04-23 03:32:15 +0100

    fix(core): remove ToArray allocations in GitBlobHasher; comment BenchmarkDotNet entry

--- src/GitDocumentDb.Core/Internal/GitBlobHasher.cs
+++ src/GitDocumentDb.Core/Internal/GitBlobHasher.cs
@@ -10,10 +10,12 @@ internal static class GitBlobHasher
         Span<byte> headerBuf = stackalloc byte[32];
         var headerLen = Encoding.ASCII.GetBytes($"blob {content.Length}\0", headerBuf);
 
-        using var sha1 = SHA1.Create();
-        sha1.TransformBlock(headerBuf[..headerLen].ToArray(), 0, headerLen, null, 0);
-        var tail = content.ToArray();
-        sha1.TransformFinalBlock(tail, 0, tail.Length);
-        return Convert.ToHexStringLower(sha1.Hash!);
+        using var hash = IncrementalHash.CreateHash(HashAlgorithmName.SHA1);
+        hash.AppendData(headerBuf[..headerLen]);
+        hash.AppendData(content);
+
+        Span<byte> digest = stackalloc byte[20];
+        hash.GetHashAndReset(digest);
+        return Convert.ToHexStringLower(digest);
     }
 }
